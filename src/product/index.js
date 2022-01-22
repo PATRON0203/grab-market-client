bbc794f02bce1ce3edff028c98dbe3c4e2adb28b
PATRON0203 <71368298+PATRON0203@users.noreply.github.com>
2022-01-22 23:14:32 +0900

    antd 기본 적용

---
+++ src/product/index.js
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
+
+function ProductPage() {
+  const { id } = useParams();
+  const [product, setProducts] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        "https://fb2a66ca-a51c-4e04-8025-4b38f3644a27.mock.pstmn.io/products/" +
+          id
+      )
+      .then(function (result) {
+        setProducts(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022년 01 월 14일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
